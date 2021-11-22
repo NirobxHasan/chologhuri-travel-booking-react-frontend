454b9f4529acb1fb16207b7145988e8a0f4a6e93
Nirob Hasan <thenirobhasan@gamil.com>
2021-11-22 13:22:26 +0600

    env changed

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,9 +1,15 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: 'AIzaSyBwrfghCYb4GBQfdwMQVnJRm1Vutnw0Zfk',
+    authDomain: 'chologhuri-travel.firebaseapp.com',
+    projectId: 'chologhuri-travel',
+    storageBucket: 'chologhuri-travel.appspot.com',
+    messagingSenderId: '612156362574',
+    appId: '1:612156362574:web:0f64ce0bb6a64da9b1181f'
 };
 export default firebaseConfig;
